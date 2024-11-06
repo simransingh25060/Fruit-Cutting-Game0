e5d84207da325258c1bae72f11198510a0c8736a
Simran Singh <simransingh25060@gmail.com>
2024-11-07 00:55:34 +0530

    Update scriptgame.js

--- scriptgame.js
+++ scriptgame.js
@@ -240,40 +240,41 @@ const colors = [
 ];
 
 circles.forEach(function (circle, index) {
-  circle.x = 0;
-  circle.y = 0;
-  circle.style.backgroundColor = colors[index % colors.length];
-});
-
-window.addEventListener("mousemove", function(e){
-  coords.x = e.clientX;
-  coords.y = e.clientY;
-  
-});
-
-function animateCircles() {
-  
-  let x = coords.x;
-  let y = coords.y;
-  
-  circles.forEach(function (circle, index) {
-    circle.style.left = x - 12 + "px";
-    circle.style.top = y - 12 + "px";
-    
-    circle.style.scale = (circles.length - index) / circles.length;
-    
-    circle.x = x;
-    circle.y = y;
-
-    const nextCircle = circles[index + 1] || circles[0];
-    x += (nextCircle.x - x) * 0.3;
-    y += (nextCircle.y - y) * 0.3;
+    circle.x = 0;
+    circle.y = 0;
+    circle.style.backgroundColor = colors[index % colors.length];
+    circle.style.transform = "scale(1)"; 
   });
- 
-  requestAnimationFrame(animateCircles);
-}
 
-animateCircles();
+  window.addEventListener("mousemove", function (e) {
+    coords.x = e.clientX;
+    coords.y = e.clientY;
+  });
 
+
+  function animateCircles() {
+    let x = coords.x;
+    let y = coords.y;
+
+    
+    circles.forEach(function (circle, index) {
+     
+      circle.style.left = x - circle.offsetWidth / 2 + "px";
+      circle.style.top = y - circle.offsetHeight / 2 + "px";
+      
+      circle.style.transform = `scale(${(circles.length - index) / circles.length})`;
+
+      circle.x = x;
+      circle.y = y;
+
+      const nextCircle = circles[index + 1] || circles[0]; 
+      x += (nextCircle.x - x) * 0.3; 
+      y += (nextCircle.y - y) * 0.3;
+    });
+
+    
+    requestAnimationFrame(animateCircles);
+  }
+  animateCircles();
 
 startGame();
